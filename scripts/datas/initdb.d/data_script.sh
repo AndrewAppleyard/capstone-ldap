e268c64e87039003adca76d40cf9494684bc864b
Andrew Appleyard <appleyardandrew0@gmail.com>
2025-10-15 22:49:20 -0500

    Messed up dn path

--- scripts/datas/initdb.d/data_script.sh
+++ scripts/datas/initdb.d/data_script.sh
@@ -2,19 +2,19 @@
 
 echo "Adding Groups"
 ldapadd -D "cn=Directory Manager" -w ${DS_DM_PASSWORD} -H ldap://localhost:3389 -x <<EOF
-dn: cn=UAFS_ADMINS,cn=groups,cn=accounts,${DS_SUFFIX_NAME}
+dn: cn=UAFS_ADMINS,cn=groups,cn=Person,${DS_SUFFIX_NAME}
 cn: UAFS_ADMINS
 objectclass: groupOfNames
 EOF
 
 ldapadd -D "cn=Directory Manager" -w ${DS_DM_PASSWORD} -H ldap://localhost:3389 -x <<EOF
-dn: cn=UAFS_ADVISORS,cn=groups,cn=accounts,${DS_SUFFIX_NAME}
+dn: cn=UAFS_ADVISORS,cn=groups,cn=Person,${DS_SUFFIX_NAME}
 cn: UAFS_ADVISORS
 objectclass: groupOfNames
 EOF
 
 ldapadd -D "cn=Directory Manager" -w ${DS_DM_PASSWORD} -H ldap://localhost:3389 -x <<EOF
-dn: cn=UAFS_STUDENTS,cn=groups,cn=accounts,${DS_SUFFIX_NAME}
+dn: cn=UAFS_STUDENTS,cn=groups,cn=Person,${DS_SUFFIX_NAME}
 cn: UAFS_STUDENTS
 objectclass: groupOfNames
 EOF
@@ -23,7 +23,7 @@ EOF
 
 echo "Adding Users"
 ldapadd -D "cn=Directory Manager" -w ${DS_DM_PASSWORD} -H ldap://localhost:3389 -x <<EOF
-dn: uid=jdoe,cn=users,cn=accounts,${DS_SUFFIX_NAME}
+dn: uid=jdoe,cn=users,cn=Person,${DS_SUFFIX_NAME}
 uid: jdoe
 givenName: John
 objectClass: inetorgperson
@@ -36,8 +36,8 @@ EOF
 
 echo "addings student to group"
 ldapmodify -D "cn=Directory Manager" -w ${DS_DM_PASSWORD} -H ldap://localhost:3389 -x <<EOF
-dn: cn=UAFS_STUDENTS,cn=groups,cn=accounts,${DS_SUFFIX_NAME}
+dn: cn=UAFS_STUDENTS,cn=groups,cn=Person,${DS_SUFFIX_NAME}
 changetype: modify
 add: member
-member: uid=jdoe,cn=users,cn=accounts,${DS_SUFFIX_NAME}
+member: uid=jdoe,cn=users,cn=Person,${DS_SUFFIX_NAME}
 EOF
