bb98a9b37b163d7d4c4a36807dd757cb8a0ba84d
Andrew Appleyard <appleyardandrew0@gmail.com>
2025-10-20 22:05:43 -0500

    better ordering

--- test.py
+++ test.py
@@ -16,15 +16,6 @@ def main():
 
     try:
         server = Server(LDAP_HOST, port=LDAP_PORT, get_info=ALL)
-
-        try:
-            user_conn = Connection(server, user=user_dn, password=password, auto_bind=True)
-            print(f"Authentication successful for {username}")
-            user_conn.unbind()
-        except Exception:
-            print(f"Authentication failed: Incorrect password for {username}")
-            return
-
         conn = Connection(server, user=LDAP_USER, password=LDAP_PASS, auto_bind=True)
 
         print("Connection successful")
@@ -35,6 +26,15 @@ def main():
         if not conn.entries:
             print("User not found")
         else:
+            
+            try:
+                user_conn = Connection(server, user=user_dn, password=password, auto_bind=True)
+                print(f"Authentication successful for {username}")
+                user_conn.unbind()
+            except Exception:
+                print(f"Authentication failed: Incorrect password for {username}")
+                return
+            
             user_dn = conn.entries[0].entry_dn
             conn.search(search_base=BASE_DN, search_filter=f"(member={user_dn})", search_scope=SUBTREE,attributes=["cn"])
 
